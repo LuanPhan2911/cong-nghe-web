4c8829ad93b20742fe1abbc4793219cbcdaa1b46
luanphan2911 <baoluan291103oe@gmail.com>
2023-08-26 16:35:45 +0700

    init home page

--- index.php
+++ index.php
@@ -4,13 +4,240 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="bootstrap/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/index.css">
     <title>Trang chủ</title>
 </head>
 
 <body>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Review Truyện</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-collapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="nav-collapse">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <main>
+            <div class="row">
+
+                <div class="col-lg-8">
+                    <div id="feature-review" class="carousel slide p-3 bg-white">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#feature-review" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#feature-review" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#feature-review" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="./images/truong-dich-review.jpeg">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>First slide label</h5>
+                                    <p>Some representative placeholder content for the first slide.</p>
+                                </div>
+                            </div>
+
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#feature-review" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#feature-review" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                    <div class="reviews p-3 bg-white my-3">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="row">
+                                    <div class="col-lg-4">
+                                        <img src="./images/review-truyen-trong-sinh-cung-truc-ma-vuong-gia-250x250.jpg" alt="" width="100%" height="100%">
+                                    </div>
+                                    <div class="col-lg-8">
+                                        <div class="name">
+                                            <a href=""> Review truyen vuong gia trong sinh</a>
+                                        </div>
+                                        <div class="author-name">
+                                            Nguyen Van Trung
+                                        </div>
+                                        <div class="created_at">
+                                            30 thang 3 2023
+                                        </div>
+                                        <div class="view">
+                                            <i class="bi bi-eye"></i>
+                                            <span>220</span>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="row">
+                                    <div class="col-lg-4">
+                                        <img src="./images/review-truyen-trong-sinh-cung-truc-ma-vuong-gia-250x250.jpg" alt="" width="100%" height="100%">
+                                    </div>
+                                    <div class="col-lg-8">
+                                        <div class="name">
+                                            <a href=""> Review truyen vuong gia trong sinh</a>
+                                        </div>
+                                        <div class="author-name">
+                                            Nguyen Van Trung
+                                        </div>
+                                        <div class="created_at">
+                                            30 thang 3 2023
+                                        </div>
+                                        <div class="view">
+                                            <i class="bi bi-eye"></i>
+                                            <span>220</span>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="row">
+                                    <div class="col-lg-4">
+                                        <img src="./images/review-truyen-trong-sinh-cung-truc-ma-vuong-gia-250x250.jpg" alt="" width="100%" height="100%">
+                                    </div>
+                                    <div class="col-lg-8">
+                                        <div class="name">
+                                            <a href=""> Review truyen vuong gia trong sinh</a>
+                                        </div>
+                                        <div class="author-name">
+                                            Nguyen Van Trung
+                                        </div>
+                                        <div class="created_at">
+                                            30 thang 3 2023
+                                        </div>
+                                        <div class="view">
+                                            <i class="bi bi-eye"></i>
+                                            <span>220</span>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="row">
+                                    <div class="col-lg-4">
+                                        <img src="./images/review-truyen-trong-sinh-cung-truc-ma-vuong-gia-250x250.jpg" alt="" width="100%" height="100%">
+                                    </div>
+                                    <div class="col-lg-8">
+                                        <div class="name">
+                                            <a href=""> Review truyen vuong gia trong sinh</a>
+                                        </div>
+                                        <div class="author-name">
+                                            Nguyen Van Trung
+                                        </div>
+                                        <div class="created_at">
+                                            30 thang 3 2023
+                                        </div>
+                                        <div class="view">
+                                            <i class="bi bi-eye"></i>
+                                            <span>220</span>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="row">
+                                    <div class="col-lg-4">
+                                        <img src="./images/review-truyen-trong-sinh-cung-truc-ma-vuong-gia-250x250.jpg" alt="" width="100%" height="100%">
+                                    </div>
+                                    <div class="col-lg-8">
+                                        <div class="name">
+                                            <a href=""> Review truyen vuong gia trong sinh</a>
+                                        </div>
+                                        <div class="author-name">
+                                            Nguyen Van Trung
+                                        </div>
+                                        <div class="created_at">
+                                            30 thang 3 2023
+                                        </div>
+                                        <div class="view">
+                                            <i class="bi bi-eye"></i>
+                                            <span>220</span>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination">
+                                <li class="page-item">
+                                    <a class="page-link" href="#" aria-label="Previous">
+                                        <span aria-hidden="true">&laquo;</span>
+                                    </a>
+                                </li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#" aria-label="Next">
+                                        <span aria-hidden="true">&raquo;</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+
+
+
+
+                <div class="col-lg-4">
+                    <div class="new-review p-2 bg-white">
+                        <ul>
+                            <li>Review truyen vuong gia trong sinh</li>
+                            <li>Review truyen vuong gia trong sinh</li>
+                            <li>Review truyen vuong gia trong sinh</li>
+                            <li>Review truyen vuong gia trong sinh</li>
+                            <li>Review truyen vuong gia trong sinh</li>
+                            <li>Review truyen vuong gia trong sinh</li>
+                            <li>Review truyen vuong gia trong sinh</li>
+                            <li>Review truyen vuong gia trong sinh</li>
+                            <li>Review truyen vuong gia trong sinh</li>
+                            <li>Review truyen vuong gia trong sinh</li>
+                            <li>Review truyen vuong gia trong sinh</li>
+                            <li>Review truyen vuong gia trong sinh</li>
+                            <li>Review truyen vuong gia trong sinh</li>
+                            <li>Review truyen vuong gia trong sinh</li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </main>
+
+        <footer>
+            <p>
+                &copy; Review Truyen
+            </p>
+        </footer>
+
+    </div>
 
     <script src="js/jquery-3.7.0.min.js"></script>
+    <script src="bootstrap/js/bootstrap.bundle.js"></script>
+
+
 </body>
 
 </html>
